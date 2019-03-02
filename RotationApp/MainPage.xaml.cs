4ef8323d7f663cbfbaf0ce1563c24421f9903a2a
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-01 21:21:48 -0400

    first commit

---
+++ RotationApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RotationApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int valor = 0;
+
+        void AdicionarValor(object sender, System.EventArgs e)
+        {
+            valor = valor+5;
+            label_rotation.Rotation = valor;
+        }
+
+        void SubtrairValor(object sender, System.EventArgs e)
+        {
+            valor = valor-5;
+            label_rotation.Rotation = valor;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
